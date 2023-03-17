05867505382111d057df71bb61bc721e370dd753
gamgammoo2 <127065256+gamgammoo2@users.noreply.github.com>
2023-03-17 14:58:36 +0900

    endian

---
+++ c/gcc/endian.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+int main() {
+  int x=1;
+
+  if (*(char *)&x =1) {
+    printf(" Result : %d \n", *(char *)&x);
+    printf(" This system is Little-End \n");
+  } else {
+    printf(" Result : %d \n", *(char *)&x);
+    printf(" This system is Big-End \n");
+  }  
+}

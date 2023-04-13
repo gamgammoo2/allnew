c8bea7a12c29082a0ad22f496e4934ce45337757
gamgammoo2 <wjdgmldus21@naver.com>
2023-04-13 15:05:40 +0900

    css_0413_1

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -73,12 +73,45 @@ app.post('/delete', (req, res) => {
 });
 //-----------------------------------------------------------------------
 
-// request 1, query 1
-app.post('/success', (req, res) => {
+// register
+app.post("/success", (req, res) => {
     const { id, pw } = req.body;
-    const result = connection.query("insert into user values (?,?)", [id, pw]);
+    if (id == "") {
+        res.redirect("register.html");
+    } else {
+        let result = connection.query("select * from user where userid=?", [id]);
+        if (result.length > 0) {
+            res.writeHead(200);
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+                <title>Error</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <div class="box3">
+                    <h3 style="margin-left: 30px">Register Failed</h3>
+                    <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.</h4>
+                    <a href="register.html" style="margin-left: 30px">다시 시도하기</a>
+                </div>
+            </body>
+            </html>
+            `;
+            res.end(template);
+        } else {
+            result = connection.query("insert into user values (? ,?)", [id, pw]);
+            console.log(result);
+            res.redirect("/");
+        }
+    }
+});
+
+// request O, query X
+app.get("/select", (req, res) => {
+    const result = connection.query("select * from user");
     console.log(result);
-    res.redirect('/success.html');
+    res.send(result);
 });
 
 // request 1, query 1
@@ -93,7 +126,7 @@ app.post('/find', (req, res) => {
         res.redirect("member.html")
     } else {
         console.log(id + " >>>> 유저 로그인함")
-        res.redirect('user.html')
+        res.redirect('gohome.html')
     }
 });
 

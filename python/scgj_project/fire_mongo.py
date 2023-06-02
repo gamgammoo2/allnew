2f96e5935b5335499d24c1fa516e08b34e1cd214
gamgammoo2 <wjdgmldus21@naver.com>
2023-06-02 17:09:50 +0900

    0602_mini2_final

--- python/scgj_project/fire_mongo.py
+++ python/scgj_project/fire_mongo.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 from fastapi.encoders import jsonable_encoder
 
 import io
-
 plt.rcParams['font.family'] = "AppleGothic"
 
 pydantic.json.ENCODERS_BY_TYPE[ObjectId] = str
@@ -42,6 +41,34 @@ print('Connected to Mongodb....')
 mydb = client['test']
 mycol = mydb['firedb']
 
+# ---sql
+import requests
+from database import db_conn
+from models import fireTotal
+from sqlalchemy import create_engine, text
+from PIL import Image
+import base64
+from io import BytesIO
+from typing import List
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+os.makedirs("./images", exist_ok=True)
+app.mount("/images", StaticFiles(directory="./images"), name='images')
+
+HOSTNAME = get_secret("Mysql_Hostname")
+PORT = get_secret("Mysql_Port")
+USERNAME = get_secret("Mysql_Username")
+PASSWORD = get_secret("Mysql_Password")
+DBNAME = get_secret("Mysql_DBname")
+
+DB_URL = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DBNAME}'
+
+engine = create_engine(DB_URL, pool_recycle=500)
+#mysql --
+
+
 def getRequestUrl(url):
     req = urllib.request.Request(url)
     try:
@@ -66,11 +93,6 @@ def duplica(mth):
     query = {"월별":mth}
     count = mycol.count_documents(query)
     return count > 0
-
-#연결 체크
-@app.get('/')
-def healthCheck():
-    return {"OK":True}
 
 #년도별 산불 발생수가 mongodb에 저장됨(중복 안되게 처리)
 @app.get('/add_fire')
@@ -101,6 +123,28 @@ async def firemongo():
     return dict_result
     # return {"OK": True, "db": "mongodb", "service": "/firemongo"}
 
+def InsertImageDB(filename):
+    os.chdir('./images')
+## jpg dpi 100x100, png dpi 72x72
+    with open(filename, "rb") as image_file:
+        binary_image = image_file.read()
+        binary_image = base64.b64encode(binary_image)
+        binary_image = binary_image.decode('UTF-8')
+        img_df = pd.DataFrame({'filename':filename,'image_data':[binary_image]})
+        #이미지의 텍스트화가 너무 길어서 longtext를 받을 수 있게 type 재정의
+        with engine.begin() as connection:
+            alter_query = text("ALTER TABLE images MODIFY image_data LONGTEXT")
+            connection.execute(alter_query)
+
+        # 중복 체크 후 데이터 삽입
+        existing_filenames = pd.read_sql_table('images', con=engine, columns=['filename'])
+        if filename in existing_filenames.values:
+            return f'"OK":False,"{filename}":"exist"'
+        else:
+            img_df.to_sql('images', con=engine, if_exists='append', index=False)
+
+    os.chdir('../')
+    return '"Image file" : "Inserted"'
 
 #mongodb에 저장된 입력한 년도에 맞아떨어지는 데이터 가져오기
 @app.get('/year_firemongo')
@@ -109,7 +153,6 @@ async def year_firemongo(year1=None,year2=None):
         return "'년도(ex,2018)의 입력을 확인해주세요"
     else:
         results=await firemongo()
-        #'inf'는 무한대를 나타내는 특수한 값을 의미
         result = [{"년도-월": key, "산불 발생 수": value if value != "-" else "0"} for key, value in results.items()]
 
         df = pd.DataFrame(result, columns = ['년도-월','산불 발생 수'])
@@ -131,7 +174,7 @@ async def year_firemongo(year1=None,year2=None):
         df_json2 = jsonable_encoder(df_year2.reset_index(drop=True))
 
         # 그래프 그리기
-        fig, ax = plt.subplots(figsize=(15, 10))
+        fig, ax = plt.subplots(figsize=(10, 6))
 
         # 입력한 연도 데이터에서 월별 데이터 추출
         x1 = df_year1['년도-월'].str[-2:].tolist()
@@ -141,7 +184,7 @@ async def year_firemongo(year1=None,year2=None):
 
         # 입력한 연도 그래프 그리기
         ax.plot(x1, y1, marker='o', label=str(year1) + '년', color='peru')
-        ax.plot(x2, y2, marker='o', label=str(year2) + '년', color='darkkhaki')
+        ax.plot(x2, y2, marker='x', label=str(year2) + '년', color='darkkhaki')
 
         ax.set_xlabel('월')
         ax.set_ylabel('산불 발생 수')
@@ -155,23 +198,26 @@ async def year_firemongo(year1=None,year2=None):
         # y축 범위 설정
         ax.set_ylim([0, max(max(y1), max(y2)) + 10])
 
-        # 각 마커 왼쪽에 값 출력
+        #o점의  왼쪽에 값 출력
         for idx, value in enumerate(y1):
             if x1[idx] in ['06', '07', '08', '09', '10', '11', '12']:
                 plt.text(x=x1[idx], y=y1[idx]+2, s=str(value), horizontalalignment='right', verticalalignment='center', color='olivedrab', fontsize=15, fontweight='bold')
             else:
                 plt.text(x=x1[idx], y=y1[idx]+1, s=str(value), horizontalalignment='right', verticalalignment='center')
 
-        # 각 마커 오른쪽에 값 출력
+        # 점의오른쪽에 값 출력
         for idx, value in enumerate(y2):
             if x2[idx] in ['06', '07', '08', '09', '10', '11', '12']:
                 plt.text(x=x2[idx], y=y2[idx]+2, s=str(value), horizontalalignment='left', verticalalignment='center', color='purple', fontsize=15, fontweight='bold')
             else:
                 plt.text(x=x2[idx], y=y2[idx]+1, s=str(value), horizontalalignment='left', verticalalignment='center')
-        save_path=f'./graph/fire/yearfire_total_{year1}&{year2}.png'
-        plt.savefig(save_path, dpi=400, bbox_inches='tight')
 
-        return df_json1,df_json2
+        save_path =f'./images/'
+        filename=f'yearfire_total_{year1}&{year2}.png'
+        plt.savefig(save_path+filename, dpi=400, bbox_inches='tight')
+        resultss=InsertImageDB(filename)
+    
+        return resultss
 
 @app.get('/combined_frame2/{year1}/{year2}')
 async def combined_frame2(year1: int, year2: int):
@@ -235,22 +281,68 @@ async def result_fire(year1=None,year2=None):
     
     
     if data1:
-        max_temp1 = max(data1.values())
+        max_fire1 = max(data1.values())
         #value의 값을 숫자로 변환
         data1_values = [int(value) for value in data1.values()]
-        avg_temp1 = sum(data1_values) / len(data1_values)
-        max_month1 = [k for k, v in data1.items() if v == max_temp1][0].split('-')[1]
-        output += f"{year1}년의 하반기(6월~12월) 중 가장 '산불 발생 수'가 높은 달은 '{max_temp1}회'인 {max_month1}월입니다. {year1}년 하반기(6월~12월)의 평균 '산불 발생 수'는 {avg_temp1:.1f}입니다."
+        avg_fire1 = sum(data1_values) / len(data1_values)
+        max_month1 = [k for k, v in data1.items() if v == max_fire1][0].split('-')[1]
+        output += f"{year1}년의 하반기(6월~12월) 중 가장 '산불 발생 수'가 높은 달은 '{max_fire1}회'인 {max_month1}월입니다. {year1}년 하반기(6월~12월)의 평균 '산불 발생 수'는 {avg_fire1:.1f}입니다."
 
     if data2:
-        max_temp2 = max(data2.values())
+        max_fire2 = max(data2.values())
         #value의 값을 숫자로 변환
         data2_values = [int(value) for value in data2.values()]
-        avg_temp2 = sum(data2_values) / len(data2_values)
-        max_month2 = [k for k, v in data2.items() if v == max_temp2][0].split('-')[1]
-        output += f"{year2}년의 하반기(6월~12월) 중 가장 '산불 발생 수'가 높은 달은 '{max_temp2}회'인 {max_month2}월입니다. {year2}년 하반기(6월~12월)의 평균 '산불 발생 수'는 {avg_temp2:.1f}입니다."
+        avg_fire2 = sum(data2_values) / len(data2_values)
+        max_month2 = [k for k, v in data2.items() if v == max_fire2][0].split('-')[1]
+        output += f"{year2}년의 하반기(6월~12월) 중 가장 '산불 발생 수'가 높은 달은 '{max_fire2}회'인 {max_month2}월입니다. {year2}년 하반기(6월~12월)의 평균 '산불 발생 수'는 {avg_fire2:.1f}입니다."
+    if avg_fire1 > avg_fire2:
+        output += f"{year1}년의 하반기에 '산불 발생 수'가 {year2}년의 하반기보다 더 많았습니다."
+
+    if avg_fire1 < avg_fire2:
+        output += f"{year2}년의 하반기에 '산불 발생 수'가 {year1}년의 하반기보다 더 많았습니다."
 
     return output
+
+db = db_conn()
+session = db.sessionmaker()
+
+#input year -> 년도별 경보 수  insert mysql
+@app.get('/mysql_fire')
+async def mysql_fire(year1=None,year2=None):
+    if year1 is None and year2 is None:
+        return "'년도(ex,2018,2019)'을 입력하세요"
+    else:
+        results=await firemongo()
+        result = [{"년도-월": key, "산불 발생 수": value if value != "-" else "0"} for key, value in results.items()]
+
+        df = pd.DataFrame(result, columns = ['년도-월','산불 발생 수'])
+    
+        # '년월' 열을 날짜형으로 변환
+        df['년도-월'] = pd.to_datetime(df['년도-월'])
+
+        # 입력한 연도 데이터 필터링
+        df_year1 = df[df['년도-월'].dt.year == int(year1)].sort_values(by='년도-월')
+        df_year2 = df[df['년도-월'].dt.year == int(year2)].sort_values(by='년도-월')
+
+        # '년도-월' 열의 형식을 월만 포함하도록 변경
+        df_year1['년도-월'] = df_year1['년도-월'].dt.strftime('%Y-%m')
+        df_year2['년도-월'] = df_year2['년도-월'].dt.strftime('%Y-%m')
+
+        #df의 index를 바꿔야함
+        df_json1 = jsonable_encoder(df_year1.reset_index(drop=True))
+        df_json2 = jsonable_encoder(df_year2.reset_index(drop=True))
+
+        #딕셔너리의 키와 값으로 zip을 생성하기
+        for (date1, count1), (date2, count2) in zip(df_year1, df_year2):
+            total = fireTotal(DATADATE1=date1, COUNT1=count1, DATADATE2=date2, COUNT2=count2)
+            session.add(total)
+            session.dirty.add(total)
+
+        session.commit()
+
+        results=session.query(fireTotal).all()
+        
+        return results
 
 #람다 없이도 컬럼 정렬하기
 # def convert_str_to_int(x):

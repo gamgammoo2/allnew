0c2035c97d60ce74b0e8e1ea05757b0e47144bb2
gamgammoo2 <wjdgmldus21@naver.com>
2023-06-29 08:26:42 +0000

    weather

---
+++ node/weather/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const app = express()
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+app.set('port', process.env.PORT || 8000)
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+//mongoose configuration
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://43.200.210.69:27017/mydb")
+mongoose.set('strictQuery',true)
+
+var main = require('./routes/main.js')
+app.use('/', main)
+
+app.listen(app.get('port'), () => {
+    console.log('8000 Port : Server Started...')
+});
+
